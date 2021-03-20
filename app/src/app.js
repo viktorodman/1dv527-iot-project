18454213280bf02e2b9840afc3f5a1ea6d2ba9ba
Viktor Ödman <vo222dq@student.lnu.se>
2021-03-20 16:52:34 +0100

    Updating server.js

--- app/src/app.js
+++ app/src/app.js
@@ -2,13 +2,29 @@ import express from express
 import logger from 'morgan'
 import session from 'express-session'
 import http from 'http'
+import helmet from 'helmet'
 import {Server } from 'socket.io'
 import { dbConnect } from './config/mongoose.js'
+import IndexRouter from './routes/indexRouter.js'
+import morgan from 'morgan'
+import dotenv from 'dotenv'
 
+dotenv.config()
 
 const main = async () => {
     await dbConnect()
 
+    const server = new Server({
+        routers: [
+            new IndexRouter('/')
+        ],
+        middleWares: [
+            helmet(),
+            morgan('dev'),
+            express.json(),
+            express.urlencoded({ extended: true })
+        ]
+    })
     
 }
 
--- app/src/server.js
+++ app/src/server.js
@@ -1,15 +1,18 @@
 import http from 'http'
 import {Server as SocketServer} from 'socket.io'
+import createError from 'http-errors'
 import { dirname, join } from 'path'
 import {fileURLToPath} from 'url' 
 import hbs from 'express-hbs'
 import { socketRouter } from './routes/socketRouter'
+import session from 'express-session'
 
 export default class Server {
     constructor(appInit) {
         this.appInit = appInit
         this.app = undefined
         this.server = undefined
+        this.baseRouter = express.Router()
     }
 
     run () {
@@ -17,6 +20,7 @@ export default class Server {
         this.setUpViewEngine()
         this.setUpSession()
         this.setUpRouter()
+        this.setUpMiddlewares()
         this.handleErrors()
         this.listen()
     }
@@ -50,15 +54,38 @@ export default class Server {
     }
 
     setUpRouter() {
+        this.app.use('/', this.baseRouter)
+        this.appInit.routers.forEach(router => this.baseRouter.use(router.baseUrl, router.expressRouter))
 
+        this.baseRouter.use('*', (req, res, next) => {
+            next(createError(404))
+        })
+    }
+
+    setUpMiddlewares() {
+        this.appInit.middleWares.forEach(mw => this.app.use(mw))
     }
 
     setUpSession() {
+        const sessionOptions = {
+            name: process.env.SESSION_NAME,
+            secret: process.env.SESSION_SECRET || "test",
+            resave: false,
+            saveUninitialized: false,
+            cookie: {
+                maxAge: 1000 * 60 * 60 * 24,
+                sameSite: 'lax',
+                httpOnly: false
+            }
+        }
 
+        this.app.use(session(sessionOptions))
     }
 
     handleErrors() {
-
+        if(err.status === 404) {
+            return res.status(404).sendFile(join(directoryFullName, 'views', 'errors', '404.html'))
+        }
     }
 
     listen() {
