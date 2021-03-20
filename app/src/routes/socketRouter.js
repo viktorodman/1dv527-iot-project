71441cafc5176702ebe414e98ca0afbd04cab853
Viktor Ödman <vo222dq@student.lnu.se>
2021-03-20 16:28:29 +0100

    Adding socketRouter

---
+++ app/src/routes/socketRouter.js
@@ -0,0 +1,11 @@
+import { Server, Socket } from "socket.io";
+
+
+export const socketRouter = (io) => {
+    io.on('connect', (socket) => {
+        console.log('User connected')
+        socket.on('disconnect', () => {
+            console.log('user disconnected')
+        })
+    })
+}
