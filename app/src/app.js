3dae92c66b6ad61639237393bf9e87c4ec09dca8
Viktor Ödman <vo222dq@student.lnu.se>
2021-03-20 16:25:39 +0100

    Adding directories views models controllers routes and utils

--- app/src/app.js
+++ app/src/app.js
@@ -1,9 +1,6 @@
 import express from express
 import logger from 'morgan'
 import session from 'express-session'
-import hbs from 'express-hbs'
-import { dirname, join } from 'path'
-import {fileURLToPath} from 'url' 
 import http from 'http'
 import {Server } from 'socket.io'
 import { dbConnect } from './config/mongoose.js'
--- app/src/server.js
+++ app/src/server.js
@@ -1,13 +1,64 @@
+import http from 'http'
+import {Server as SocketServer} from 'socket.io'
+import { dirname, join } from 'path'
+import {fileURLToPath} from 'url' 
+import hbs from 'express-hbs'
+
 export default class Server {
     constructor(appInit) {
         this.appInit = appInit
+        this.app = undefined
+        this.server = undefined
     }
 
     run () {
         this.setUpSocket()
         this.setUpViewEngine()
+        this.setUpSession()
         this.setUpRouter()
         this.handleErrors()
         this.listen()
+    }
+
+    setUpSocket() {
+        this.app = express()
+        this.server = http.createServer(this.app)
+
+        const io = new SocketServer(this.server)
+
+        this.app.use((req, res, next) => {
+            res.io = io
+
+            next()
+        })
+    }
+
+    setUpViewEngine() {
+        const directoryFullName = dirname(fileURLToPath(import.meta.url))
+
+        const baseURL = process.env.BASE_URL || '/'
+
+        this.app.engine('hbs', hbs.express4({
+            defaultLayout: join(directoryFullName, 'views', 'layouts', 'default'),
+            partialsDir: join(directoryFullName, 'view', 'partials')
+        }))
+        this.app.set('view engine', 'hbs')
+        this.app.set('views', join(directoryFullName), 'views')
+    }
+
+    setUpRouter() {
+
+    }
+
+    setUpSession() {
+
+    }
+
+    handleErrors() {
+
+    }
+
+    listen() {
+
     }
 }
